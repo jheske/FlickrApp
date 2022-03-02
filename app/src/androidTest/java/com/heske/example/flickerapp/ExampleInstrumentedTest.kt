fd7f26b37e8fdc93c247e06ec2a3b6895da866ed
Jill Heske <jheske@users.noreply.github.com>
2022-03-02 10:12:29 -0500

    Add clickButton() test

--- app/src/androidTest/java/com/heske/example/flickerapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/heske/example/flickerapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.heske.example.flickerapp
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.heske.example.flickerapp.ui.main.MainActivity
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import com.heske.example.flickerapp.R
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +22,14 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Rule @JvmField
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun clickButton() {
+       onView(withId(R.id.searchView)).perform(click()).check(matches(isDisplayed()))
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
--- app/src/main/java/com/heske/example/flickerapp/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/heske/example/flickerapp/ui/detail/DetailActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
